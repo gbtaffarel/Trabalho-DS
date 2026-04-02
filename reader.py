d24fbb13de3a03285df9263d783b5d0489b7a713
Gabriel <gb.taffarel@gmail.com>
2026-04-02 00:50:02 -0300

    Adiciona alguns comentários

--- reader.py
+++ reader.py
@@ -1,20 +1,24 @@
-class reader:
+# Esse modulo eh responsavel por ler o arquivo de entrada do programa
 
+
+class reader:
     def __init__(self, path):
-        self.path = path
+        self.path = path  # caminho do arquivo de entrada
         self.content = ""
-        self.pos = 0
+        self.pos = 0  # Posicao do cursor de leitura
 
     def load(self):
         with open(self.path, "r") as f:
             self.content = f.read()
 
+    # Metodo para iterar sobre a string, caracter por caracter
     def next(self):
         if self.pos < len(self.content):
             c = self.content[self.pos]
             self.pos += 1
             return c
         return None
-    
+
     def length(self):
         return len(self.content)
+
--- translator.py
+++ translator.py
@@ -1,7 +1,7 @@
+# Esse modulo contem o conjunto de regras para traduriz os caracteres
+# em notas, instrumentos, oitavas e volume.
 class translator:
-
     def __init__(self):
-        
         self.notas = {
             # Notas padroes
             "C": 0,
@@ -11,47 +11,46 @@ class translator:
             "G": 7,
             "A": 9,
             "B": 11,
-
             # Notas sustenidas / bemol
-            "H": 10 # Si Bemol
+            "H": 10,  # Si Bemol
         }
 
     def translate(self, char):
         if char in self.notas:
             teste = self.notas.get(char)
             return teste
-    
+
+        # Transforma o caractere para minusculo para facilitar comparacoes
         c = char.lower()
 
         if c in "abcdefgh":
             return ["volume", 0]
-    
+
         if c == " ":
             return ["volume", 2]
-    
+
         if c == "!":
             return ["instrument", 24]
-    
+
         if c in "oiu":
             return ["instrument", 110]
-    
+
         if c in "jklmnpqrstvwxyz":
             return ["consonant"]
-    
+
         if isinstance(c, int) and c % 2 == 0:
             return ["instrument+", c]
-    
+
         if c == "?":
             return ["oitava"]
-    
+
         if c == "\n":
             return ["instrument", 123]
-    
+
         if c == ";" or isinstance(c, int) and c % 2 != 0:
             return ["instrument", 15]
-    
+
         if c == ",":
             return ["instrument", 114]
-    
+
         return ["consonant"]
-    
